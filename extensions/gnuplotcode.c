c38b744fcf629aebaae1d7c046d0a71ad2ae3f52
Nimish Bongale <nimishnb98@gmail.com>
2019-02-06 01:20:36 +0530

    Add files via upload

---
+++ extensions/gnuplotcode.c
@@ -0,0 +1,12 @@
+int main()
+{
+/*FILE *gnuplot = popen("gnuplot -persistent", "w");
+	fprintf(gnuplot, "plot '-'\n");
+	for (i = 0; i < testrun; i++)
+    fprintf(gnuplot, "%g %g\n", x[i], y[i]);
+	fprintf(gnuplot, "e\n");
+	fflush(gnuplot);*/
+
+return 0;
+}
+
