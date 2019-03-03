8a0429e34a19b18ad347fd6f198fc51b3c6019c8
Nimish Bongale <nimishnb98@gmail.com>
2019-03-04 00:23:28 +0530

    Add files via upload

---
+++ public/bfsnet.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
+
+
+void bfs(int v) {
+
+ for(i = 1; i <= n; i++)
+ 	if(a[v][i] && !visited[i])
+ 	q[++r] = i;
+ 
+ if(f <= r) {
+ 	visited[q[f]] = 1;
+ 	bfs(q[f++]);
+ }
+}
+
+int main() {
+ int v;
+ printf("Enter the number of vertices:");
+ scanf("%d", &n);
+ for(i=1; i <= n; i++) {
+ 	q[i] = 0;
+ 	visited[i] = 0;
+}
+ printf("\nEnter the adjacency matrix:\n");
+ for(i=1; i<=n; i++) {
+ 	for(j=1;j<=n;j++) {
+ 		scanf("%d", &a[i][j]);
+ }
+}
+ printf("\nEnter the starting vertex:");
+ scanf("%d", &v);
+ bfs(v);
+ printf("\nThe nodes which are reachable are:\n");
+ for(i=1; i <= n; i++) 
+ 	if(visited[i])
+ 		printf("%d\t", i);
+	
+	return 0;
+ }
+
+
